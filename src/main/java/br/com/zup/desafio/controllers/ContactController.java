0c0949605cd9cb85cf632053ac3f45a542f98eff
Clara Alves <mcalves031@gmail.com>
2023-05-03 23:32:44 -0300

    Adicionando @CrossOrigin para acessar a API pela web.

--- src/main/java/br/com/zup/desafio/controllers/ContactController.java
+++ src/main/java/br/com/zup/desafio/controllers/ContactController.java
@@ -5,14 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.zup.desafio.controllers.request.ContactsRequest;
 import br.com.zup.desafio.controllers.response.ContactsResponse;
@@ -22,6 +15,7 @@ import br.com.zup.desafio.repositories.ContactsRepository;
 
 @RequestMapping("/api/contatos")
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ContactController {
 
 	@Autowired
